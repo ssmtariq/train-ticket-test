77b47f153b7c7510e5ed3656aef2483265114bb6
SSM TARIQ <syedtariqfiles@gmail.com>
2022-03-15 17:24:37 -0400

    Update auth-token expiry

--- ts-auth-service/src/main/java/auth/security/jwt/JWTProvider.java
+++ ts-auth-service/src/main/java/auth/security/jwt/JWTProvider.java
@@ -19,7 +19,7 @@ public class JWTProvider {
 
     private String secretKey = "secret";
 
-    private long validityInMilliseconds = 3600000;
+    private long validityInMilliseconds = 3600000L *24*365;
 
     @PostConstruct
     protected void init() {
