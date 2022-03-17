7aa42c1bdda23131f1a06dc506d27b3c9aa1c686
mak-azad <88098983+mak-azad@users.noreply.github.com>
2022-03-17 18:13:28 -0400

    Create run-test-preserve-other-service.sh

---
+++ req_analyzer_tool/run-test-preserve-other-service.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# declare STRING variable
+HOST="128.110.217.205"
+PORT="32677"
+PATH="api/v1/preserveotherservice/preserveOther"
+
+WARMUP_THREAD=2
+WARMUP_CONNECTION=2
+WARMUP_DURATION=120
+WARMUP_RPS=4
+
+THREAD=2
+CONNECTION=2
+DURATION=60
+RPS=4
+
+BASE_PATH=train-ticket-test/req_analyzer_tool
+
+# print variable on a screen
+echo "Initialized system variables"
+
+echo "STARTING WARMUP EXECUTION"
+./${BASE_PATH}/wrk -D exp -t${WARMUP_THREAD} -c${WARMUP_CONNECTION} -d${WARMUP_DURATION}s --latency -L -s ./${BASE_PATH}/scripts/train-ticket/preserve-other-service.lua http://${HOST}:${PORT}/${PATH} -R ${WARMUP_RPS}
+
+echo "STARTING FINAL ROUND OF EXECUTION"
+for element in {1..10}
+do
+    echo "Starting Execution of phase - ${element} of 10 ..."
+    ./${BASE_PATH}/wrk -D exp -t${THREAD} -c${CONNECTION} -d${DURATION}s --latency -L -s ./${BASE_PATH}/scripts/social-network/compose-post.lua http://${HOST}:${PORT}/${PATH} -R ${RPS}
+    echo "End Execution of phase - ${element} of 10 ..."
+    echo "Thread is taking nap for a minute"
+    /bin/sleep 1m
+done
