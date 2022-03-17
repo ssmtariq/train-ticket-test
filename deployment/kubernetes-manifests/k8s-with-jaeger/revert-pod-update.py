9a3aa94211b8589baace2f51e775efe5adf4db5c
mak-azad <88098983+mak-azad@users.noreply.github.com>
2022-03-17 17:43:36 -0400

    Create revert-pod-update.py

---
+++ deployment/kubernetes-manifests/k8s-with-jaeger/revert-pod-update.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import time
+
+print("Reverting pod update ts-train-service")
+os.system("kubectl set image deployment/ts-train-service -n=default ts-train-service=codewisdom/ts-train-service-with-jaeger:v1")
+time.sleep(60)
+print("Reverting pod update ts-travel2-service")
+os.system("kubectl set image deployment/ts-travel2-service -n=default codewisdom/ts-travel2-service-with-jaeger:v1")
+time.sleep(5)
+print("Updating finished!")
