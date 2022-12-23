7ef02a773a68e1c79e0e97d5e6c550562d38cfcb
ssmtariq <syedtariqfiles@gmail.com>
2022-12-23 18:29:01 -0500

    Update deployment scripts, pom and annotation for SPERF for searchQuickestResponse

--- ts-route-plan-service/src/main/java/plan/service/RoutePlanServiceImpl.java
+++ ts-route-plan-service/src/main/java/plan/service/RoutePlanServiceImpl.java
@@ -27,7 +27,6 @@ public class RoutePlanServiceImpl implements RoutePlanService {
     private static final Logger LOGGER = LoggerFactory.getLogger(RoutePlanServiceImpl.class);
 
     @Override
-    @SperfAnno(monitorObject="searchCheapestResult")
     public Response searchCheapestResult(RoutePlanInfo info, HttpHeaders headers) {
 
         //1.Violence pulls out all the results of travel-service and travle2-service
@@ -85,6 +84,7 @@ public class RoutePlanServiceImpl implements RoutePlanService {
     }
 
     @Override
+    @SperfAnno(monitorObject="TripResponse")
     public Response searchQuickestResult(RoutePlanInfo info, HttpHeaders headers) {
 
         //1.Violence pulls out all the results of travel-service and travle2-service
