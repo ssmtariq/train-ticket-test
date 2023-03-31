5cf4d4340765d485da512704197acc8a072e6688
SSM TARIQ <syedtariqfiles@gmail.com>
2023-03-31 09:58:30 -0400

    Add more highspeed trip data

--- ts-travel-service/src/main/java/travel/init/InitData.java
+++ ts-travel-service/src/main/java/travel/init/InitData.java
@@ -75,5 +75,106 @@ public class InitData implements CommandLineRunner{
         info.setStartingTime(new Date("Mon May 04 07:00:00 GMT+0800 2013")); //NOSONAR
         info.setEndTime(new Date("Mon May 04 19:59:52 GMT+0800 2013")); //NOSONAR
         service.create(info,null);
+
+        /*Add new Highspeed Trip*/
+        info.setTripId("G1238");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("314db8ba-06fb-4a6a-aadb-70f3dc88a745");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1239");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("6f91eee5-47c3-4796-b593-4914ef104eea");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1240");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("9201643b-7e5c-4775-bd3c-a1cc8a06887d");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1241");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("a98dad5e-7ae7-4969-b2e1-142884f1c345");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1242");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("80e1b43d-1826-4435-a3ab-ade07ced880c");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1243");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("d5127cd7-67db-4415-9651-73fd27025a7b");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1244");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("7e4e6fc8-5a62-407c-981b-22ee6636e472");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1245");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("8a80284f-cbc4-467e-809f-7a857a6875f2");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1246");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("85cd1d9b-0e47-4013-ad5b-a936f1bb2158");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
+
+        info.setTripId("G1247");
+        info.setTrainTypeId(gaoTieOne);
+        info.setRouteId("079527ba-40ab-49a1-a81f-5fd9136d676d");
+        info.setStartingStationId(shanghai);
+        info.setStationsId(suzhou);
+        info.setTerminalStationId(taiyuan);
+        info.setStartingTime(new Date("Mon May 04 14:00:00 GMT+0800 2013")); //NOSONAR
+        info.setEndTime(new Date("Mon May 04 20:51:52 GMT+0800 2013")); //NOSONAR
+        service.create(info,null);
     }
 }
