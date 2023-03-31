356bd00a3e51c9afb97c3d53358f4b545e1b231f
SSM TARIQ <syedtariqfiles@gmail.com>
2023-03-31 07:32:45 -0400

    Add more routes for highspeed and normal trains

--- ts-route-service/src/main/java/route/init/InitData.java
+++ ts-route-service/src/main/java/route/init/InitData.java
@@ -92,6 +92,149 @@ public class InitData implements CommandLineRunner {
         info.setStationList("shanghai,suzhou");
         info.setDistanceList("0,50");
         routeService.createAndModify(info,null);
+        
+        /*Adding new highspeed train route*/
+
+        info.setId("314db8ba-06fb-4a6a-aadb-70f3dc88a745");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("6f91eee5-47c3-4796-b593-4914ef104eea");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("9201643b-7e5c-4775-bd3c-a1cc8a06887d");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("80e1b43d-1826-4435-a3ab-ade07ced880c");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("d5127cd7-67db-4415-9651-73fd27025a7b");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("7e4e6fc8-5a62-407c-981b-22ee6636e472");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("8a80284f-cbc4-467e-809f-7a857a6875f2");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("85cd1d9b-0e47-4013-ad5b-a936f1bb2158");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("079527ba-40ab-49a1-a81f-5fd9136d676d");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        info.setId("a98dad5e-7ae7-4969-b2e1-142884f1c345");
+        info.setStartStation(nanjing);
+        info.setEndStation(shanghai);
+        info.setStationList("nanjing,suzhou,shanghai");
+        info.setDistanceList("0,200,250");
+        routeService.createAndModify(info,null);
+
+        /*Adding new normal train route*/
+        info.setId("26e335c6-1efa-468e-a404-6f813248798b");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("7b8c0886-1dc9-42a7-b1c0-ae351cd8e683");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("88b74fde-db80-477c-9bee-2ef3d8fe5b19");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("6ba359a7-4de2-4810-9bc8-28a94a3c3848");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("39bfb3b3-9369-40c4-9c93-b5b6ee5dd76d");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("ebfea9d2-b1e8-40e8-a8d1-f20b4897e4f4");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("4813b2da-cb02-4824-bf05-3cfcdee1da61");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("ba43ab68-978c-4d51-b3fb-26766d909c7b");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("a11f1e49-f24a-4db1-9eff-a47cc2a9afc4");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
+
+        info.setId("89de9a9b-5108-48c1-a564-b02833ebfe39");
+        info.setStartStation(taiyuan);
+        info.setEndStation(shanghai);
+        info.setStationList("taiyuan,shijiazhuang,nanjing,shanghai");
+        info.setDistanceList("0,300,950,1300");
+        routeService.createAndModify(info,null);
 
     }
 
